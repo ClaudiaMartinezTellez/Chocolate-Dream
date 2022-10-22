a132b35ea4162f4232ee3ca7fb9c51add52f6e3b
ClaudiaMartinezTellez <112780006+ClaudiaMartinezTellez@users.noreply.github.com>
2022-10-22 12:53:57 -0500

    5 imagenes
    Se agregaron al Stock 5 imagenes con sus correspondientes descripciones en la linea 10 a la 14 y see corrigieron las rutas de las img.

--- stock.js
+++ stock.js
@@ -7,4 +7,9 @@ let stockProductos = [
     {id: 6, nombre: "Mini Emogis 1", tipo: "paleta", cantidad: 1, desc: "Paleta de chocolate y ceral", precio: 1000, img: './img/MiniEmogisPaletas.jpeg'},
     {id: 7, nombre: "Mini Magnum 1", tipo: "paleta", cantidad: 1, desc: "Paleta de chocolate con amarnato y arroz inflado cubiertas de chocolate", precio: 1000, img: './img/MiniMagnumAmaranto.jpeg'},
     {id: 8, nombre: "Paletas Personalizada 1", tipo: "Corazon", cantidad: 1, desc: "Paleta en forma de corazon con frase personalizada, chocolate semi-amargo", precio: 1000, img: "./img/PaletasPersonalizadasCereal.jpeg"},
+    {id: 9, nombre: "Brocheta de bombon 1", tipo: "Brocheta", cantidad: 1, desc: "Bombon cubierto de chocolate, decorado con granillo de chocolate o nuez", precio: 1000, img: "./img/bombon.jpg"},
+    {id: 10, nombre: "Paletas Toda Ocacion 1", tipo: "Paleta", cantidad: 1, desc: "paleta chocolate macizo", precio: 1000, img: './img/chocolate-paletas.jpg'},
+    {id: 11, nombre: "Manzanas cubiertas 1", tipo: "Manzana", cantidad: 1, desc: "Manzana cubierta de chocolate", precio: 1000, img: './img/manzanas-chocolate.jpg'},
+    {id: 12, nombre: "Magnum Pinguino 1", tipo: "Paleta", cantidad: 1, desc: "Paleta rellena de pan de chocolate", precio: 1000, img: './img/choco-pinguino.jpg'},
+    {id: 13, nombre: "Enjambre de chocolate 1", tipo: "enjambre", cantidad: 1, desc: "Enjambre de cereal cubierto de chocolate", precio: 1000, img: './img/enjambre-de-chocolate.jpg'},
 ] //esto es un objeto json
